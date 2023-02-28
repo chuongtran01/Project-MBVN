3f66cb1e1c18066c8916729d4747d88feeabb967
Thong Hoang <14352@student.vgu.edu.vn>
2023-02-28 10:12:47 +0700

    Add Create and Delete for Doctor Management

---
+++ Areas/Admin/Models/CreateDoctorModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HospitalManagementSystem.Areas.Admin.Models
+{
+    public class CreateDoctorModel
+{
+        public string? Name { get; set; }
+        public string? Field { get; set; }
+        public string? Gender { get; set; }
+        public DateTime? Birthday { get; set; }
+        [EmailAddress(ErrorMessage = "Please enter valid Email address")]
+        public string? EmailAddress { get; set; }
+        [Phone(ErrorMessage = "Please enter valid Phone number")]
+        public string? PhoneNumber { get; set; }
+        public string? Password { get; set; }
+        public int? DepartmentId { get; set; }
+    }
+}
